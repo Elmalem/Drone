f4a00a3008354c0bda22862856128a68c07b709b
Elmalem <44621326+Elmalem@users.noreply.github.com>
2020-04-02 09:50:32 +0300

    Upload Config

--- src/Config.java
+++ src/Config.java
@@ -1,4 +1,6 @@
 
 public class Config {
 	public static String path = "C:\\\\Users\\\\user\\\\git\\\\Matala.1";
+	public static String author1 = "Snir sharbani";
+	public static String author2 = "Lioz elmalem";
 }
